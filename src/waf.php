0f97f83963ddf9f9d9c4fe923e72188d3a1f430e
Matheus Zych <mzych@databay.de>
2024-01-19 15:02:37 +0100

    Added the ability to activate and deactivate the WAF

--- src/waf.php
+++ src/waf.php
@@ -5,18 +5,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Entity\Request;
 use App\Handler\RequestHandler;
 use App\Service\ConfigLoader;
+
 define('CONFIG', ConfigLoader::loadConfig());
 
-$request = new Request(
-    $_REQUEST,
-    $_GET,
-    $_POST,
-    $_FILES,
-    $_COOKIE,
-    $_SESSION,
-    $_SERVER
-);
+if (defined('CONFIG') && isset(CONFIG['WAF_ACTIVE']) && CONFIG['WAF_ACTIVE'] === 'true') {
+    $request = new Request(
+        $_REQUEST,
+        $_GET,
+        $_POST,
+        $_FILES,
+        $_COOKIE,
+        $_SESSION,
+        $_SERVER
+    );
 
-$filtersOutput = RequestHandler::handleRequest($request);
+    $filtersOutput = RequestHandler::handleRequest($request);
 
-echo $filtersOutput ? 'Not filtered' : 'Filtered';
+    echo $filtersOutput ? 'Not filtered' : 'Filtered';
+}

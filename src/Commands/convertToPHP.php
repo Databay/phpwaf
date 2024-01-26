adfc3ed5e867d010558f73b42d14f08b6ccddd96
Matheus Zych <mzych@databay.de>
2024-01-26 14:58:59 +0100

    Improved UX of convertToPHP

--- src/Commands/convertToPHP.php
+++ src/Commands/convertToPHP.php
@@ -1,35 +1,37 @@
 <?php
 if ($argc < 3) {
-    echo 'Usage: php ' . basename(__FILE__) . ' <relative/path/to/file.txt> <relative/path/to/newFileName.php>' . PHP_EOL; exit(1);
+    echo 'Usage: php ' . basename(__FILE__) . ' <path/to/file.txt> <path/to/newFileName.php>' . PHP_EOL; exit(1);
 }
 
 array_shift($argv);
 list($filePath, $newFilePath) = $argv;
 
+$newFilePath = trim($newFilePath, '/');
+$newFullFilePath = __DIR__ . '/../../' . $newFilePath;
 $data = <<<END
 <?php
 return [
 END;
 
-file_put_contents(__DIR__ . '/' . $newFilePath, $data . PHP_EOL);
+file_put_contents($newFullFilePath, $data . PHP_EOL);
 
-foreach (file(__DIR__ . '/' . $filePath) as $line) {
+foreach (file(__DIR__ . '/../../' . trim($filePath, '/')) as $line) {
     $line = trim($line);
     $line = trim($line, PHP_EOL);
     $line = str_replace(['\\', '\''], ['\\\\', '\\\''], $line);
 
     if (!isset($values[$line])) {
         $values[$line] = false;
-        file_put_contents(__DIR__ . '/' . $newFilePath, "\t" . '\'' . $line . '\' => \'' .$line . '\',' . PHP_EOL, FILE_APPEND);
+        file_put_contents($newFullFilePath, "\t" . '\'' . $line . '\' => \'' .$line . '\',' . PHP_EOL, FILE_APPEND);
     }
 }
 
-file_put_contents(__DIR__ . '/' . $newFilePath, '];', FILE_APPEND);
+file_put_contents($newFullFilePath, '];', FILE_APPEND);
 
 try {
-    $array = require_once(__DIR__ . '/' .$newFilePath);
+    $array = require_once($newFullFilePath);
 } catch (Throwable $e) {
-    unlink($newFilePath);
+    unlink($newFullFilePath);
     echo 'An error occurred. Please check the files and try again.' . PHP_EOL; exit(1);
 }
 

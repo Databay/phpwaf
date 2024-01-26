ed484fa029a93c788185a06dc323a7deb166d767
Matheus Zych <mzych@databay.de>
2024-01-26 15:30:49 +0100

    Extended BaseTestCase

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -3,9 +3,28 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionMethod;
 
 class BaseTestCase extends TestCase
 {
+    private float $startTime;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->startTime = microtime(true);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $endTime = microtime(true);
+        $this->assertLessThan( 0.01, ($endTime - $this->startTime), 'Took too long. From: ' . $this->startTime . ' to ' . $endTime);
+    }
+
     protected function assertArrayContainsInstanceOf(string|object $expected, array $actual): void
     {
         if (is_object($expected)) {
@@ -20,5 +39,20 @@ class BaseTestCase extends TestCase
         }
 
         $this->fail();
+    }
+
+    protected static function getMethod(string|object $classOrObject, string $method): ReflectionMethod
+    {
+        $reflectionClass = new ReflectionClass($classOrObject);
+        $method = $reflectionClass->getMethod($method);
+        $method->setAccessible(true);
+        return $method;
+    }
+
+    protected function replaceStaticMethod(string $className, string $methodName, $mock): void
+    {
+        $reflectionClass = new ReflectionClass($className);
+        $reflectionMethod = $reflectionClass->getMethod($methodName);
+        $reflectionMethod->setAccessible(true);
     }
 }

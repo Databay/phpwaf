42b8f07948b3d2e7dbe17902eeceae1703ec1427
Matheus Zych <mzych@databay.de>
2024-01-29 16:33:39 +0100

    Made AbstractPayloadFilter unit tests fully working (proud)

--- tests/Abstracts/AbstractPayloadFilterTest.php
+++ tests/Abstracts/AbstractPayloadFilterTest.php
@@ -1,155 +1,167 @@
 <?php
 
-namespace Abstracts;
+namespace App\Tests\Abstracts {
+    use App\Abstracts\AbstractPayloadFilter;
+    use App\Entity\Request;
+    use App\Tests\BaseTestCase;
+    use PHPUnit\Framework\Attributes\DataProvider;
+    use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
-use App\Abstracts\AbstractPayloadFilter;
-use App\Entity\Request;
-use App\Tests\BaseTestCase;
-use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
-
-#[RunTestsInSeparateProcesses]
-class AbstractPayloadFilterTest extends BaseTestCase
-{
-    private AbstractPayloadFilter $abstractPayloadFilter;
-
-    protected function setUp(): void
+    #[RunTestsInSeparateProcesses]
+    class AbstractPayloadFilterTest extends BaseTestCase
     {
-        parent::setUp();
+        private AbstractPayloadFilter $abstractPayloadFilter;
 
-        $this->abstractPayloadFilter = new class extends AbstractPayloadFilter {
-            public $filterName;
+        protected function setUp(): void
+        {
+            parent::setUp();
 
-            public $criticalMatch;
+            $this->abstractPayloadFilter = new class extends AbstractPayloadFilter {
+                public $filterName;
 
-            public function apply(Request $request): bool
-            {
-                return true;
-            }
-        };
+                public $criticalMatch;
+
+                public function apply(Request $request): bool
+                {
+                    return true;
+                }
+            };
+        }
+
+        #[DataProvider('valueFoundInPayloadDataProvider')]
+        public function testValueFoundInPayload(array $input, bool $output): void
+        {
+            $method = self::getMethod($this->abstractPayloadFilter, 'valueFoundInPayload');
+            $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, $input));
+        }
+
+        public static function valueFoundInPayloadDataProvider(): array
+        {
+            return [
+                [['test', ['test' => 'test'], true], true],
+                [['test', ['test' => 'test'], false], true],
+                [['test', [], true], false],
+                [['test', [], false], false],
+                [[['test'], ['test' => 'test'], true], true],
+                [[['test'], ['test' => 'test'], false], true],
+                [[['test'], [], true], false],
+                [[['test'], [], false], false],
+                [['prefixtestsuffix', ['test' => 'test'], true], false],
+                [['prefixtestsuffix', ['test' => 'test'], false], true],
+                [['prefixtestsuffix', [], true], false],
+                [['prefixtestsuffix', [], false], false],
+                [[['prefixtestsuffix'], ['test' => 'test'], true], false],
+                [[['prefixtestsuffix'], ['test' => 'test'], false], true],
+                [[['prefixtestsuffix'], [], true], false],
+                [[['prefixtestsuffix'], [], false], false],
+                [['test', ['prefixtestsuffix' => 'prefixtestsuffix'], true], false],
+                [['test', ['prefixtestsuffix' => 'prefixtestsuffix'], false], false],
+                [['test', [], true], false],
+                [['test', [], false], false],
+                [[['test'], ['prefixtestsuffix' => 'prefixtestsuffix'], true], false],
+                [[['test'], ['prefixtestsuffix' => 'prefixtestsuffix'], false], false],
+                [[['test'], [], true], false],
+                [[['test'], [], false], false],
+            ];
+        }
+
+        #[DataProvider('recursiveArrayTraversalDataProvider')]
+        public function testRecursiveArrayTraversal(array $input, bool $output): void
+        {
+            $method = self::getMethod($this->abstractPayloadFilter, 'recursiveArrayTraversal');
+            $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, $input));
+        }
+
+        public static function recursiveArrayTraversalDataProvider(): array
+        {
+            return [
+                [[['test'], ['test' => 'test'], true], true],
+                [[['test'], ['test' => 'test'], false], true],
+                [[['test'], [], true], false],
+                [[['test'], [], false], false],
+                [[['prefixtestsuffix'], ['test' => 'test'], true], false],
+                [[['prefixtestsuffix'], ['test' => 'test'], false], true],
+                [[['prefixtestsuffix'], [], true], false],
+                [[['prefixtestsuffix'], [], false], false],
+            ];
+        }
+
+        #[DataProvider('handlePayloadDataProvider')]
+        public function testHandleCriticalPayload(array $input, bool $output): void
+        {
+            $this->abstractPayloadFilter->filterName = 'ABSTRACTPAYLOAD';
+            define('CONFIG', [
+                'FILTER_ABSTRACTPAYLOAD_CRITICAL_PAYLOAD_FILES' => $input['payloadFileString'],
+                'FILTER_ABSTRACTPAYLOAD_CRITICAL_STRICT_MATCH' => $input['strictMatch'],
+            ]);
+            $method = self::getMethod($this->abstractPayloadFilter, 'handleCriticalPayload');
+            $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, [$input['value']]));
+        }
+
+        #[DataProvider('handlePayloadDataProvider')]
+        public function testHandleRegularPayload(array $input, bool $output): void
+        {
+            $this->abstractPayloadFilter->filterName = 'ABSTRACTPAYLOAD';
+            define('CONFIG', [
+                'FILTER_ABSTRACTPAYLOAD_PAYLOAD_FILES' => $input['payloadFileString'],
+                'FILTER_ABSTRACTPAYLOAD_STRICT_MATCH' => $input['strictMatch'],
+            ]);
+            $method = self::getMethod($this->abstractPayloadFilter, 'handleRegularPayload');
+            $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, [$input['value']]));
+        }
+
+        public static function handlePayloadDataProvider(): array
+        {
+            return [
+                [['value' => 'test', 'payloadFileString' => '[]', 'strictMatch' => 'true'], true],
+                [['value' => 'test', 'payloadFileString' => '[test]', 'strictMatch' => 'true'], false],
+                [['value' => 'test', 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'true'], true],
+                [['value' => 'test', 'payloadFileString' => '[INVALID]', 'strictMatch' => 'true'], true],
+                [['value' => ['test'], 'payloadFileString' => '[]', 'strictMatch' => 'true'], true],
+                [['value' => ['test'], 'payloadFileString' => '[test]', 'strictMatch' => 'true'], false],
+                [['value' => ['test'], 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'true'], true],
+                [['value' => ['test'], 'payloadFileString' => '[INVALID]', 'strictMatch' => 'true'], true],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[]', 'strictMatch' => 'true'], true],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[test]', 'strictMatch' => 'true'], true],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'true'], false],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[INVALID]', 'strictMatch' => 'true'], true],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[]', 'strictMatch' => 'true'], true],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[test]', 'strictMatch' => 'true'], true],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'true'], false],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[INVALID]', 'strictMatch' => 'true'], true],
+
+                [['value' => 'test', 'payloadFileString' => '[]', 'strictMatch' => 'false'], true],
+                [['value' => 'test', 'payloadFileString' => '[test]', 'strictMatch' => 'false'], false],
+                [['value' => 'test', 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'false'], true],
+                [['value' => 'test', 'payloadFileString' => '[INVALID]', 'strictMatch' => 'false'], true],
+                [['value' => ['test'], 'payloadFileString' => '[]', 'strictMatch' => 'false'], true],
+                [['value' => ['test'], 'payloadFileString' => '[test]', 'strictMatch' => 'false'], false],
+                [['value' => ['test'], 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'false'], true],
+                [['value' => ['test'], 'payloadFileString' => '[INVALID]', 'strictMatch' => 'false'], true],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[]', 'strictMatch' => 'false'], true],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[test]', 'strictMatch' => 'false'], false],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'false'], false],
+                [['value' => 'prefixtestsuffix', 'payloadFileString' => '[INVALID]', 'strictMatch' => 'false'], true],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[]', 'strictMatch' => 'false'], true],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[test]', 'strictMatch' => 'false'], false],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[prefixtestsuffix]', 'strictMatch' => 'false'], false],
+                [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[INVALID]', 'strictMatch' => 'false'], true],
+            ];
+        }
     }
+}
 
-    #[DataProvider('valueFoundInPayloadDataProvider')]
-    public function testValueFoundInPayload(array $input, bool $output): void
+namespace App\Service {
+    class PayloadLoader
     {
-        $method = self::getMethod($this->abstractPayloadFilter, 'valueFoundInPayload');
-        $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, $input));
-    }
-
-    public static function valueFoundInPayloadDataProvider(): array
-    {
-        return [
-            [['test', ['test' => 'test'], true], true],
-            [['test', ['test' => 'test'], false], true],
-            [['test', [], true], false],
-            [['test', [], false], false],
-            [[['test'], ['test' => 'test'], true], true],
-            [[['test'], ['test' => 'test'], false], true],
-            [[['test'], [], true], false],
-            [[['test'], [], false], false],
-            [['prefixtestsuffix', ['test' => 'test'], true], false],
-            [['prefixtestsuffix', ['test' => 'test'], false], true],
-            [['prefixtestsuffix', [], true], false],
-            [['prefixtestsuffix', [], false], false],
-            [[['prefixtestsuffix'], ['test' => 'test'], true], false],
-            [[['prefixtestsuffix'], ['test' => 'test'], false], true],
-            [[['prefixtestsuffix'], [], true], false],
-            [[['prefixtestsuffix'], [], false], false],
-            [['test', ['prefixtestsuffix' => 'prefixtestsuffix'], true], false],
-            [['test', ['prefixtestsuffix' => 'prefixtestsuffix'], false], false],
-            [['test', [], true], false],
-            [['test', [], false], false],
-            [[['test'], ['prefixtestsuffix' => 'prefixtestsuffix'], true], false],
-            [[['test'], ['prefixtestsuffix' => 'prefixtestsuffix'], false], false],
-            [[['test'], [], true], false],
-            [[['test'], [], false], false],
-        ];
-    }
-
-    #[DataProvider('recursiveArrayTraversalDataProvider')]
-    public function testRecursiveArrayTraversal(array $input, bool $output): void
-    {
-        $method = self::getMethod($this->abstractPayloadFilter, 'recursiveArrayTraversal');
-        $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, $input));
-    }
-
-    public static function recursiveArrayTraversalDataProvider(): array
-    {
-        return [
-            [[['test'], ['test' => 'test'], true], true],
-            [[['test'], ['test' => 'test'], false], true],
-            [[['test'], [], true], false],
-            [[['test'], [], false], false],
-            [[['prefixtestsuffix'], ['test' => 'test'], true], false],
-            [[['prefixtestsuffix'], ['test' => 'test'], false], true],
-            [[['prefixtestsuffix'], [], true], false],
-            [[['prefixtestsuffix'], [], false], false],
-        ];
-    }
-/*
-    TODO: Fix this test when able
-*/
-//    #[DataProvider('handleCriticalPayloadDataProvider')]
-//    public function testHandleCriticalPayload(array $input, bool $output): void
-//    {
-//        $this->abstractPayloadFilter->filterName = 'ABSTRACTPAYLOAD';
-//        define('CONFIG', ['FILTER_ABSTRACTPAYLOAD_CRITICAL_PAYLOAD_FILES' => $input['payloadFileString']]);
-//        $method = self::getMethod($this->abstractPayloadFilter, 'handleCriticalPayload');
-//        $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, [$input['value']]));
-//    }
-
-    public static function handleCriticalPayloadDataProvider(): array
-    {
-        return [
-            [['value' => 'test', 'payloadFileString' => '[]'], true],
-            [['value' => 'test', 'payloadFileString' => '[test]'], true],
-            [['value' => 'test', 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => 'test', 'payloadFileString' => '[INVALID]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[test]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[INVALID]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[test]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[INVALID]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[test]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[INVALID]'], true],
-        ];
-    }
-/*
-    TODO: Fix this test when able
-*/
-//    #[DataProvider('handleRegularPayloadDataProvider')]
-//    public function testHandleRegularPayload(array $input, bool $output): void
-//    {
-//        $this->abstractPayloadFilter->filterName = 'ABSTRACTPAYLOAD';
-//        define('CONFIG', ['FILTER_ABSTRACTPAYLOAD_PAYLOAD_FILES' => $input['payloadFileString']]);
-//        $method = self::getMethod($this->abstractPayloadFilter, 'handleRegularPayload');
-//        $this->assertEquals($output, $method->invokeArgs($this->abstractPayloadFilter, [$input['value']]));
-//    }
-
-    public static function handleRegularPayloadDataProvider(): array
-    {
-        return [
-            [['value' => 'test', 'payloadFileString' => '[]'], true],
-            [['value' => 'test', 'payloadFileString' => '[test]'], true],
-            [['value' => 'test', 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => 'test', 'payloadFileString' => '[INVALID]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[test]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => ['test'], 'payloadFileString' => '[INVALID]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[test]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => 'prefixtestsuffix', 'payloadFileString' => '[INVALID]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[test]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[prefixtestsuffix]'], true],
-            [['value' => ['prefixtestsuffix'], 'payloadFileString' => '[INVALID]'], true],
-        ];
+        public static function loadPayload(string $path): array
+        {
+            $pathExploded = explode('/', $path);
+            return match (end($pathExploded)) {
+                'test' => ['test' => 'test'],
+                'prefixtestsuffix' => ['prefixtestsuffix' => 'prefixtestsuffix'],
+                default => [],
+            };
+        }
     }
 }
